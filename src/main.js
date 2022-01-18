9a978948d010ab34fdb90350fc447691835ff93b
Jenni <jenni@ohsomodern.org>
2022-01-18 15:28:11 +0200

    :heavy_plus_sign: Add Tailwind

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import store from './store';
 import router from './router';
 
+import './styles/styles.css';
+
 const app = createApp({
   render() {
     return h(App);
