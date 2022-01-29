7998d41eb05aaf669797cd8fb4979bedbbe41297
Jenni <jenni@ohsomodern.org>
2022-01-29 10:01:34 +0200

    :lipstick: Refactor dark theme styling

--- src/views/RecipeSingle/RecipeSingle.vue
+++ src/views/RecipeSingle/RecipeSingle.vue
@@ -150,7 +150,7 @@ h3 {
 }
 
 .with-bg {
-  @apply bg-slate-50 dark:bg-slate-900 dark:text-slate-400;
+  /* bg from https://heropatterns.com/ */
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23ff002f' fill-opacity='0.06' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 }
 </style>
