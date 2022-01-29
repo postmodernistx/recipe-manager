b02eed3bc37c072aefee414ec4fc74049004ddf8
Jenni <jenni@ohsomodern.org>
2022-01-29 20:33:50 +0200

    :construction_worker: Test build

--- vite.config.github.js
+++ vite.config.github.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://postmodernistx.github.io/recipe-manager/',
+  base: '/recipe-manager/',
   build: {
     outDir: './docs',
   },
